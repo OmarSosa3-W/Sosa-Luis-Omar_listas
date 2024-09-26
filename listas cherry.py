28fe5968b0ecf20d770a17292f1bf147932b718b
OmarSosa3-W <a.23308051281217@cbtis128.edu.mx>
2024-09-25 20:52:41 -0600

    Add files via upload

---
+++ listas cherry.py
@@ -0,0 +1,23 @@
+#para practicar las listas 
+print(" ")#se coloca para dejar un espacio
+print("SOSA LUIS OMAR 3-W: MI PRACTICA DE LISTAS(VECTORES)")#nombre del creador
+print(" ")#se coloca para dejar un espacio
+
+frutas = ["apple", "banana", "cherry", "orange", "kiwi"]#definir variables
+print(frutas[2:3])#colocar el numero de la variable 
+print(" ")#se coloca para dejar un espacio
+print("E numero 3 es la fruta que se mostro en pantalla")#muestra el resultado en pantalla
+
+#cuando usamos (len()) te despliega 
+#el numero de elementos de la lista 
+frutas = ["apple", "banana", "cherry"]#definir variables
+print(len(frutas))#muestra el resultado
+
+print("la primera variable es apple")#muestra el resultado en pantalla
+print (" ")#se coloca para dejar un espacio
+print("la segunda variable es banana")#muestra el resultado en pantalla
+print(" ")#se coloca para dejar un espacio
+print("la tercera variable es cherry")#muestra el resultado en pantalla
+print(" ")#se coloca para dejar un espacio
+print("esas son las variables que mostro en pantalla")#muestra el resultado en pantalla
+print(" ")#se coloca para dejar un espacio
